527e918d06e219f6ec0d5d36bfba885996672d2f
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-03-02 17:24:59 +0400

    have added style module for Card components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import Card from "./components/Card";
+import Index from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
+
+const arr = [
+    {
+        title: "Мужские Кроссовки Nike Blazer Mid Suede",
+        price: 12999,
+        img: "/img/all_sneakers/sneakers_1.jpg"
+    },
+    {
+        title: "Мужские Кроссовки Nike Air Max 270",
+        price: 16999,
+        img: "/img/all_sneakers/sneakers_2.jpg"
+    },
+    {
+        title: "Мужские Кроссовки Nike Blazer Mid Suede",
+        price: 8999,
+        img: "/img/all_sneakers/sneakers_3.jpg"
+    },
+    {
+        title: "Кроссовки Puma X Aka Boku Future Rider",
+        price: 5999,
+        img: "/img/all_sneakers/sneakers_4.jpg"
+    }
+    ]
 
 function App() {
   return (
     <div className="wrapper clear">
         <Drawer/>
-
         <Header/>
-
       <div className="content p-40">
      <div className="mb-40 d-flex align-center justify-between">
          <h1>
@@ -20,7 +42,15 @@ function App() {
          </div>
      </div>
           <div className="d-flex">
-            <Card/>
+              {
+                  arr.map(item=>
+                      (<Index
+                          title = {item.title}
+                          price={item.price}
+                          img = {item.img}
+                          onClick = {()=> console.log(item)}
+                      />))
+              }
           </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,26 @@
-function Card(){
-    return(<div className="card">
+import cardStyles from "./Card.module.scss";
+
+function Index({title, price, img, onClick}){
+
+    const onClickButton = ()=>{
+        alert("hello")
+    }
+    return(<div className={cardStyles.card}>
         <div className='favorite'>
             <img src="/img/heart_unliked.svg" alt="heart"/>
         </div>
-        <img width={133} height={112} src="/img/all_sneakers/sneakers_1.jpg" alt="sneakers"/>
-        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+        <img width={133} height={112} src={img} alt="sneakers"/>
+        <h5>{title}</h5>
         <div className="d-flex justify-between align-center">
             <div className='d-flex flex-column'>
                 <span>price</span>
-                <b>12, 999 rub</b>
+                <b>{price} rub</b>
             </div>
-            <button className='button'>
+            <button className='button' onClick={onClick}>
                 <img width={11} height={11} src="/img/plus_button.svg" alt="plus"/>
             </button>
         </div>
     </div>)
 }
 
-export default Card
+export default Index
