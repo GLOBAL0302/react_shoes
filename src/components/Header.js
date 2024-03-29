f496796a3ee9846620d5472614cdc95b9abd50a0
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-03-29 16:02:29 +0400

    continue with the old version

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     return (
         <header className="d-flex justify-between align-center p-40">
             <div className='d-flex align-center'>
-                <Link to="">
+                <Link to="/">
                     <img width={40} height={40} src="/img/logo_shoes.png" alt=""/>
                     <div className="headerInfo">
                         <h3 className='text-uppercase'>React Snikers</h3>
